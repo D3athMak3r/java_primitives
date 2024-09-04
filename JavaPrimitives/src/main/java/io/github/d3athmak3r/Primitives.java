a5dcd986564c07626030e884a6fbfa2f0079ad94
Marcos <gomezmarcos20018@gmail.com>
2024-09-04 10:05:08 -0400

    These are the 8 Java Primitives

---
+++ JavaPrimitives/src/main/java/io/github/d3athmak3r/Primitives.java
@@ -0,0 +1,16 @@
+package io.github.d3athmak3r;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Primitives {
+    public static void main(String[] args) {
+        int a = 5;
+        short b = 2;
+        long c = 700000000000L;
+        float t = 2.44f;
+        double d = 2.44;
+        char r = 'b';
+        boolean z = true;
+        byte g= 125;
+        }
+    }
